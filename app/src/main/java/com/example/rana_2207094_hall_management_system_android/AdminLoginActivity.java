309234f487491afbb87de050acb7798cbe3d0f93
Rana094 <ranaislam135790@gmail.com>
2026-01-08 14:29:36 +0600

    Admin Login Page design and both activity is created

---
+++ app/src/main/java/com/example/rana_2207094_hall_management_system_android/AdminLoginActivity.java
@@ -0,0 +1,94 @@
+package com.example.rana_2207094_hall_management_system_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    private EditText etUsernameAdmin;
+    private EditText etPasswordAdmin;
+    private CheckBox cbShowPassword;
+    private Button btnLoginAdmin;
+    private Button btnHome;
+
+    private final String PASS = "admin";
+    private final String USERNAME = "admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login); // Link to the XML created above
+
+        etUsernameAdmin = findViewById(R.id.etUsernameAdmin);
+        etPasswordAdmin = findViewById(R.id.etPasswordAdmin);
+        cbShowPassword = findViewById(R.id.cbShowPassword);
+        btnLoginAdmin = findViewById(R.id.btnLoginAdmin);
+        btnHome = findViewById(R.id.btnHome);
+
+        cbShowPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                    etPasswordAdmin.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+
+                    etPasswordAdmin.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+
+                etPasswordAdmin.setSelection(etPasswordAdmin.getText().length());
+            }
+        });
+
+        btnLoginAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performLogin();
+            }
+        });
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoHome();
+            }
+        });
+    }
+
+    private void performLogin() {
+        String inputUsername = etUsernameAdmin.getText().toString();
+        String inputPassword = etPasswordAdmin.getText().toString();
+
+        if (!inputPassword.equals(PASS) || !inputUsername.equals(USERNAME)) {
+            showAlert("Error", "Passwords or Username do not match");
+            return;
+        }
+
+        Intent intent = new Intent(AdminLoginActivity.this, AdminHomeActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void gotoHome() {
+        Intent intent = new Intent(AdminLoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void showAlert(String title, String msg) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton("OK", null)
+                .show();
+    }
+}
--- app/src/main/java/com/example/rana_2207094_hall_management_system_android/MainActivity.java
+++ app/src/main/java/com/example/rana_2207094_hall_management_system_android/MainActivity.java
@@ -1,24 +1,75 @@
 package com.example.rana_2207094_hall_management_system_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etRollNumber, etPassword;
+    private CheckBox cbShowPassword;
+    private Button btnLogin, btnRegister, btnAdminLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        etRollNumber = findViewById(R.id.etRollNumber);
+        etPassword = findViewById(R.id.etPassword);
+        cbShowPassword = findViewById(R.id.cbShowPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnRegister = findViewById(R.id.btnGoToRegister);
+
+        btnAdminLogin = findViewById(R.id.btnGoToAdmin);
+
+        cbShowPassword.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+
+                etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+
+                etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            String roll = etRollNumber.getText().toString();
+            String pass = etPassword.getText().toString();
+
+            if(roll.isEmpty() || pass.isEmpty()){
+                Toast.makeText(MainActivity.this, "Please enter Username and Password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(MainActivity.this, "Logging in...", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, StudentHomeActivity.class);
+            intent.putExtra("USER_ROLL", roll); // Passing the roll number to the next screen
+            startActivity(intent);
+        });
+
+        btnRegister.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Going to Register Page", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        btnAdminLogin.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Going to Admin Login", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
+            startActivity(intent);
         });
     }
 }
